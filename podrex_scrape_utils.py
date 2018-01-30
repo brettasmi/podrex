44578baaea137ade6b6b898b5abd97778779294e
brettasmi <brett.a.smi@gmail.com>
2018-01-30 10:54:17 -0800

    bugfix on metadata description

--- podrex_scrape_utils.py
+++ podrex_scrape_utils.py
@@ -135,14 +135,10 @@ def parse_metadata(podcast_data, page_data, podcast_name, podcast_id,
         podcast_dict["artist_name"] = podcast_data["artistName"] # varchar
         try:
             podcast_dict["description"] = podcast_data["description"]["standard"] #text
+
         except:
-            try:
-                logging.exception("failed to get podcast description normal way")
-                podcast_dict["description"] = (' '.join("{!s}={!s}".format(k,v)
-                                               for (k,v) in podcast_data["description"].items())) # text
-            except:
-                logging.exception("failed to get description completely")
-                podcast_dict["description"] = "none from itunes-new"
+            logging.exception("failed to get description completely")
+            podcast_dict["description"] = "none from itunes-new"
         podcast_dict["feed_url"] = podcast_data["feedUrl"] # text
         podcast_dict["mean_rating"] = (podcast_data["userRating"]
                                        ["ariaLabelForRatings"].split()[0]) # float
