25861408dbada43a99e9ebb0878927565d9cef3c
brettasmi <brett.a.smi@gmail.com>
2018-03-09 11:54:33 -0800

    fix typo

--- transcode_deepspeech.sh
+++ transcode_deepspeech.sh
@@ -5,5 +5,5 @@
 
 for file in ./*.mp3
 do
-    ffmpeg - i "$file" -acodec pcm_s16le -ac 1 -ar 16000 "${file[@]/%mp3/wav}"
+    ffmpeg -i "$file" -acodec pcm_s16le -ac 1 -ar 16000 "${file[@]/%mp3/wav}"
 done
