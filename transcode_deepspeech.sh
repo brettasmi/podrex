58cb871856ac8262a750f0a212d85fbfe6944e34
brettasmi <brett.a.smi@gmail.com>
2018-06-27 12:20:06 -0700

    remove transcode_deepspeech

--- transcode_deepspeech.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# converts all mp3s in a directory to 16khz 16bit mono wavs compatible with
-# many speech recognition software
-
-for file in ./*.mp3
-do
-    ffmpeg -i "$file" -acodec pcm_s16le -ac 1 -ar 16000 "${file[@]/%mp3/wav}"
-done
