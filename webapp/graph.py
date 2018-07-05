702810668215bcd7b7d62510b4ef4702a6e18990
brettasmi <brett.a.smi@gmail.com>
2018-07-05 14:37:43 -0700

    add five_by_nlp and get_listener_overlap functions

--- webapp/graph.py
+++ webapp/graph.py
@@ -3,9 +3,7 @@
 import pickle
 
 def five_by_listeners(conn, podcast):
-    """Returns the five podcasts with the most shared
-    listeners
-    """
+    """Returns the five podcasts with the most shared listeners"""
     cursor = conn.cursor()
     five_pods = []
     cursor.execute("SELECT * FROM relationships "
@@ -21,3 +19,22 @@ def five_by_listeners(conn, podcast):
             five_pods.append((result[0], result[2]))
     cursor.close()
     return five_pods
+
+def five_by_nlp(podcast, lookup_dict, dist_matrix):
+    """Returns `podcast`'s five most closely related podcasts and their shared
+    listener count
+    """
+    return [lookup_dict[i] for i in np.argsort(dist_matrix[podcast, :])[-5:]]
+
+def get_listener_overlap(conn, podcast_1, podcast_2):
+    """Returns the count of listeners shared by podcast_1 and podcast_2"""
+    cursor = conn.cursor()
+    cursor.execute("SELECT listeners FROM relationships "
+                    "WHERE ((podcast_1 = (%s) AND podcast_2 = (%s)) "
+                    "OR ((podcast_1 = (%s) AND podcast_2 = (%s))",
+                    (podcast_1, podcast_2, podcast_2, podcast_1))
+    result = cursor.fetchone()
+    if result:
+        cursor.close()
+        return result[0]
+    cursor.close()
