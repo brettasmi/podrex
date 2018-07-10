502399cf388a293a8c2642b6076811b4ac8243b1
brettasmi <brett.a.smi@gmail.com>
2018-07-10 10:08:29 -0700

    add functions to get new podcasts

--- webapp/graph.py
+++ webapp/graph.py
@@ -23,25 +23,53 @@ class d3Graph:
             self.graph = graph
             self.nodes = graph["nodes"].copy()
         else:
-            five_pods.append((result[0], result[2]))
-    cursor.close()
-    return five_pods
+            self.graph = {"edges": [], "nodes": []}
+            self.nodes = set(nodes)
 
-def five_by_nlp(podcast, lookup_dict, dist_matrix):
-    """Returns `podcast`'s five most closely related podcasts and their shared
-    listener count
-    """
-    return [lookup_dict[i] for i in np.argsort(dist_matrix[podcast, :])[-5:]]
+    def five_by_listeners(self, conn, podcast):
+        """Returns the five podcasts with the most shared listeners"""
+        self.new_nodes = []
+        podcast = self.lookup_dict[podcast]
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM relationships "
+                       "WHERE (podcast_1 = (%s)) "
+                       "OR (podcast_2 = (%s)) "
+                       "ORDER BY listeners DESC "
+                       "LIMIT 5", (podcast, podcast))
+        results = cursor.fetchall()
+        for result in results:
+            if result[0] == podcast:
+                self.nodes.append(self.lookup_dict[result[1]])
+                self.new_nodes.append(self.lookup_dict[result[1]])
+            else:
+                self.nodes.append(self.lookup_dict[result[0]])
+                self.new_nodes.append(self.lookup_dict[result[0]])
+        cursor.close()
 
-def get_listener_overlap(conn, podcast_1, podcast_2):
-    """Returns the count of listeners shared by podcast_1 and podcast_2"""
-    cursor = conn.cursor()
-    cursor.execute("SELECT listeners FROM relationships "
-                    "WHERE ((podcast_1 = (%s) AND podcast_2 = (%s)) "
-                    "OR ((podcast_1 = (%s) AND podcast_2 = (%s))",
-                    (podcast_1, podcast_2, podcast_2, podcast_1))
-    result = cursor.fetchone()
-    if result:
+    def five_by_nlp(self, podcast, dist_matrix):
+        """Returns `podcast`'s five most closely related podcasts and their shared
+        listener count. Will not work for spark_pid > 3010.
+        """
+        self.new_nodes = []
+        if podcast > 3010:
+            return
+        for pod in np.argsort(dist_matrix[podcast, :])[:5]:
+            self.nodes.append(int(pod))
+            self.new_nodes.append(int(pod))
+
+    def _get_listener_overlap(self, conn, podcast_1, podcast_2):
+        """Returns the count of listeners shared by podcast_1 and podcast_2"""
+        podcasts = [self.lookup_dict[i] for i in
+            [podcast_1, podcast_2, podcast_2, podcast_1]]
+        cursor = conn.cursor()
+        cursor.execute("SELECT listeners FROM relationships "
+                        "WHERE (podcast_1 = (%s) AND podcast_2 = (%s)) "
+                        "OR (podcast_1 = (%s) AND podcast_2 = (%s))",
+                        podcasts)
+        result = cursor.fetchone()
+        if result:
+            cursor.close()
+            return (result[0])
         cursor.close()
         return result[0]
     cursor.close()
